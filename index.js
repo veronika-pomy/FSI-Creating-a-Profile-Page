774ea2eae4c83f42e1904f0f4b8c8e23acbecdf3
VVPCodes <pomyateevav@gmail.com>
2022-09-22 21:40:02 -0400

    made changes to js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+function runMeWhenYouCallOnMe(){
+
 
 let content = document.querySelector('.js-generated.content')
 
@@ -13,10 +15,73 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
+let dogHeader = document.createElement('h3')
+dogHeader.append('Description:')
+dogDetails.append(dogHeader)
+
+let dogParagraph = document.createElement('p')
+dogParagraph.append('This gentle dog is aloof toward her owner, and never comes when called.She always acts as though any stranger she meets will harm her and dislikes other animals.')
+dogDetails.append(dogParagraph)
+
+let dogFeeding = document.createElement('h3')
+dogFeeding.append('Feeding Times:')
+dogDetails.append(dogFeeding)
+
+//let dogList = document.createElement('ul')
+// dogList.setAttribute('class', 'dog-list')
+//dogDetails.append(dogList)
+
+
+//let dog1E = document.createElement('li')
+//dog1E.append('9:00 am')
+//dogList.append(dog1E)
+//
+//let dog2E = document.createElement('li')
+//dog2E.append('12:00 pm')
+//dogList.append(dog2E)
+
+//let dog3E = document.createElement('li')
+//dog3E.append('5:00 pm')
+//dogList.append(dog3E)
+
+let ul = document.createElement("ul");
+let dogTimes = ["9.00 am", "12:00 pm", "5:00 pm", "8:30 pm"];
+for (i = 0; i < dogTimes.length; i++){
+    let li = document.createElement("li");
+    li.textContent = dogTimes[i];
+    ul.append(li);
+}
+
+dogDetails.append(ul);
+
+}
+
+let button = document.createElement("button");
+button.textContent = "Click Me!";
+button.style.fontSize = "50px";
+document.querySelector("body").append(button);
+
+
+button.addEventListener("click",function(){
+    console.log("It was clicked.");
+    runMeWhenYouCallOnMe();
+
+})
+
+
+
+// dogBullets[i].append("dogTimes[i]")
+// dogList.append("dogBullets")
+//}
+//
+//
+//
+//
+//
 
